bfb46a13217963e9224c2da4e06b602efe6f9e34
Mr-milky-way <admilkyway@gmail.com>
2026-02-23 17:04:20 -0800

    Add project files.

---
+++ Patch_BaseUri.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.IO;
+using drl.backend;
+
+namespace DRL_DLL_Hooks
+{
+    [BepInPlugin("drl.hooks", "DRL Hooks", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogInfo("DRL hook loaded");
+
+            var harmony = new Harmony("drl.hooks");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(DRLService), "get_baseUri")]
+    class Patch_BaseUri
+    {
+        static bool Prefix(ref string __result)
+        {
+            __result = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "apiurl.txt"));
+            return false;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DRL_DLL_Hooks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("DRL_DLL_Hooks")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e2199b7-4457-49e1-84d2-8626759330c9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
